b94092ed7e9d1ee1038d4d17429e67d29f161eaf
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-04-18 09:15:35 +0200

    got  different color baground each other row to work

--- script.js
+++ script.js
@@ -36,27 +36,32 @@ function preparePostData(dataObject) {
 function showPosts(posts) {
   for (const post of posts) {
     addPostToDom(post);
-    giveDifferentBackgroundColorDependingOnId(post);
   }
 }
 //class="grid-item ${pokemon.type.toLowerCase()}"
 function addPostToDom(post) {
   const html =
     /*html */
-    `<tr id="row" >
-        <td> ${post.id} </td>
-        <td  > <img id="image" src="${post.image}"></td>
+    `<tr class="row" >
+        
+        <td> <img id="image" src="${post.image}"></td>
         <td id="title-text">${post.title}</td>
     </tr>`;
-  document.querySelector("#table").insertAdjacentHTML("beforeend", html);
+  document.querySelector("tbody").insertAdjacentHTML("beforeend", html);
+    // giveDifferentBackgroundColorDependingOnId(post);
+ 
+//   $(function () {
+//     $("table tr:even").addClass("even");
+//     $("table tr:odd").addClass("odd");
+//   });
+
 }
 
 function giveDifferentBackgroundColorDependingOnId(post) {
-  const idNumber = `${post.id}`;
-  if (idNumber % 2 === 0) {
-    document.querySelector("#row").classList.add("even");
+  if (`${post.id}` % 2 === 0) {
+    document.querySelector(".row").classList.add("even");
   } else {
-    document.querySelector("#row").classList.add("odd");
+    document.querySelector(".row").classList.add("odd");
   }
   console.log(`idNumber: ${post.id} remainder: ${post.id % 2}`);
 }

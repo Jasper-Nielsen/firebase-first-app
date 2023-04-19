0281d1ab517ec2cced068807635999c98971de11
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-04-19 18:35:29 +0200

    got buttons delete update on table

--- script.js
+++ script.js
@@ -5,20 +5,88 @@ window.addEventListener("load", initApp);
 const endpoint =
   "https://test-d2238-default-rtdb.europe-west1.firebasedatabase.app";
 
-async function initApp() {
-  const postList = await getPosts();
-  const posts = showPosts(postList);
-  //   giveDifferentBackgroundColorDependingOnId(posts)
+function initApp() {
+  updatePostsGrid();
+
+  //   createPost("laa", "text");
+  //   const userList = await getUsers();
+  //   showUsers(userList);
+
+  //   console.log(stringify(user));
 }
 
 async function getPosts() {
   const response = await fetch(`${endpoint}/posts.json`);
   const data = await response.json();
-
   const posts = preparePostData(data);
-  console.log(posts);
+  //   console.log(posts);
   return posts;
 }
+
+async function updatePostsGrid() {
+  const postList = await getPosts();
+  showPosts(postList);
+}
+async function getUsers() {
+  const response = await fetch(`${endpoint}/users.json`);
+  const data = await response.json();
+  const users = preparePostData(data);
+  //   console.log(`users: ${users}`);
+  return users;
+}
+
+// === CREATE (POST) === //
+async function createPost(title, image, body) {
+  const newPost = {
+    title: title,
+    body: body,
+    image: image,
+  };
+
+  //indhold som creates. gjort til strengform og JSON format
+  const postAsJson = JSON.stringify(newPost);
+
+  const response = await fetch(`${endpoint}/posts.json`, {
+    method: "POST",
+    body: postAsJson,
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+// === UPDATE (PUT) === //
+async function updatePost(id, title, image) {
+  const postToUpdate = { title, image };
+  const postAsJson = JSON.stringify(postToUpdate);
+  const url = `${endpoint}/posts/${id}.json`;
+
+  const res = await fetch(url, { method: "PUT", body: postAsJson });
+  const data = await res.json();
+  console.log(data);
+}
+
+// === DELETE (DELETE) === //
+
+async function deletePost(id) {
+  const url = `${endpoint}/posts/${id}.json`;
+  const response = await fetch(url, { method: "DELETE" });
+  console.log(response);
+  if (response.ok) {
+    updatePostsGrid();
+  }
+}
+
+// function parseJSONString(object) {
+//   const jsonParsed = JSON.parse(object);
+//   return jsonParsed;
+// }
+
+// function stringify(object) {
+//   const jsonString = JSON.stringify(object);
+//   console.log(jsonString);
+//   return jsonString;
+// }
 
 function preparePostData(dataObject) {
   const postArray = [];
@@ -26,14 +94,15 @@ function preparePostData(dataObject) {
   for (const key in dataObject) {
     const post = dataObject[key];
     post.id = key;
-    console.log(post);
+    // console.log(post);
     postArray.push(post);
   }
-  console.log(postArray);
+  //   console.log(postArray);
   return postArray;
 }
 
 function showPosts(posts) {
+  document.querySelector("#posts").innerHTML = "";
   for (const post of posts) {
     addPostToDom(post);
   }
@@ -43,25 +112,52 @@ function addPostToDom(post) {
   const html =
     /*html */
     `<tr class="row" >
-        
         <td> <img id="image" src="${post.image}"></td>
         <td id="title-text">${post.title}</td>
+        <td> ${post.body}</td>
+        <td>
+            <button class="btn-delete">Delete</button>
+            <button class="btn-update">Update</button>
+        </td>
+           
     </tr>`;
-  document.querySelector("tbody").insertAdjacentHTML("beforeend", html);
-    // giveDifferentBackgroundColorDependingOnId(post);
- 
-//   $(function () {
-//     $("table tr:even").addClass("even");
-//     $("table tr:odd").addClass("odd");
-//   });
+  document.querySelector("#posts").insertAdjacentHTML("beforeend", html);
 
+  document
+    .querySelector("#posts tr:last-child .btn-delete")
+    .addEventListener("click", deleteClicked);
+
+  document
+    .querySelector("#posts tr:last-child .btn-update")
+    .addEventListener("click", updateClicked);
+
+  function deleteClicked() {
+    deletePost(post.id);
+  }
+
+  function updateClicked() {
+    updatePost(post.id, title, body, image);
+  }
 }
 
-function giveDifferentBackgroundColorDependingOnId(post) {
-  if (`${post.id}` % 2 === 0) {
-    document.querySelector(".row").classList.add("even");
-  } else {
-    document.querySelector(".row").classList.add("odd");
+function showUsers(users) {
+  for (const user of users) {
+    addUserToDom(user);
   }
-  console.log(`idNumber: ${post.id} remainder: ${post.id % 2}`);
+}
+
+function addUserToDom(user) {
+  const html =
+    /*html */
+    `<tr class="row">
+        <td id="user-image"> <img src="${user.image}"></td>
+        <td id="user-mail"> ${user.mail}</td>
+        <td id="user-name">${user.name}</td>
+        <td id="user-phone">${user.phone}</td>
+        <td id="user-title">${user.title}</td>
+    </tr>
+
+   `;
+
+  document.querySelector("#users").insertAdjacentHTML("beforeend", html);
 }

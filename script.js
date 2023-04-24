628bf34cd0593d356a8381594bfb12ffd608c601
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-04-24 20:57:59 +0200

    got the delete and update to work
    removed my html form for update into the function addPostToDOM. it solved the issue

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const endpoint =
 function initApp() {
   updatePostsGrid();
 
-  //   createPost("laa", "text");
-  //   const userList = await getUsers();
-  //   showUsers(userList);
-
-  //   console.log(stringify(user));
+  document.querySelector("#btn-create").addEventListener("click", createPost);
+}
+async function updatePostsGrid() {
+  const postList = await getPosts();
+  showPosts(postList);
 }
 
 async function getPosts() {
@@ -20,13 +20,10 @@ async function getPosts() {
   const data = await response.json();
   const posts = preparePostData(data);
   //   console.log(posts);
+
   return posts;
 }
 
-async function updatePostsGrid() {
-  const postList = await getPosts();
-  showPosts(postList);
-}
 async function getUsers() {
   const response = await fetch(`${endpoint}/users.json`);
   const data = await response.json();
@@ -35,34 +32,31 @@ async function getUsers() {
   return users;
 }
 
+// function createPostClicked(){
+//   document.querySelector("#btn-create").addEventListener("click",createPost);
+//   console.log("Create clicked")
+// }
+
 // === CREATE (POST) === //
-async function createPost(title, image, body) {
+async function createPost() {
+  const elements = document.querySelector("#form-create").elements;
   const newPost = {
-    title: title,
-    body: body,
-    image: image,
+    image: elements.namedItem("image").value,
+    title: elements.namedItem("title").value,
+    body: elements.namedItem("body").value,
+    uid: elements.namedItem("uid").value,
   };
 
   //indhold som creates. gjort til strengform og JSON format
   const postAsJson = JSON.stringify(newPost);
-
+  console.log(`jason data: ${postAsJson}`);
   const response = await fetch(`${endpoint}/posts.json`, {
     method: "POST",
     body: postAsJson,
   });
 
   const data = await response.json();
-  console.log(data);
-}
-
-// === UPDATE (PUT) === //
-async function updatePost(id, title, image) {
-  const postToUpdate = { title, image };
-  const postAsJson = JSON.stringify(postToUpdate);
-  const url = `${endpoint}/posts/${id}.json`;
-
-  const res = await fetch(url, { method: "PUT", body: postAsJson });
-  const data = await res.json();
+  // updatePostsGrid(); ????
   console.log(data);
 }
 
@@ -76,17 +70,6 @@ async function deletePost(id) {
     updatePostsGrid();
   }
 }
-
-// function parseJSONString(object) {
-//   const jsonParsed = JSON.parse(object);
-//   return jsonParsed;
-// }
-
-// function stringify(object) {
-//   const jsonString = JSON.stringify(object);
-//   console.log(jsonString);
-//   return jsonString;
-// }
 
 function preparePostData(dataObject) {
   const postArray = [];
@@ -107,8 +90,9 @@ function showPosts(posts) {
     addPostToDom(post);
   }
 }
-//class="grid-item ${pokemon.type.toLowerCase()}"
+
 function addPostToDom(post) {
+  //moving the whole dialog inside this method specifically in the const html fixes issue. now it only changes the one i choose
   const html =
     /*html */
     `<tr class="row" >
@@ -119,25 +103,123 @@ function addPostToDom(post) {
             <button class="btn-delete">Delete</button>
             <button class="btn-update">Update</button>
         </td>
-           
+           <td>
+    <dialog id="update">
+      <form method="dialog" id="form-update">
+        <fieldset>
+          <legend>Update post</legend>
+          <label for="image">Update Image</label>
+          <input type="url" name="image" id="input-img" /> <br />
+          <label for="title">Update Title</label>
+          <input type="text" name="title" id="input-title" /> <br />
+          <label for="body">Update Body</label>
+          <input type="text" name="body" id="input-body" />
+        </fieldset>
+
+        <button id="close-buttton">Close</button>
+        <button type="submit" id="btn-submit">Submit</button>
+      </form>
+    </dialog>
+           </td>
     </tr>`;
   document.querySelector("#posts").insertAdjacentHTML("beforeend", html);
 
+  //makes delebutton clickable
   document
     .querySelector("#posts tr:last-child .btn-delete")
     .addEventListener("click", deleteClicked);
 
+  //makes update button clickable
   document
     .querySelector("#posts tr:last-child .btn-update")
     .addEventListener("click", updateClicked);
+
+    document.querySelector("#btn-create").addEventListener("click",createPostClicked);
+
+function createPostClicked(event){
+event.preventDefault();
+
+  document.querySelector("#form-create").reset();
+
+document.querySelector("#create-dialog").showModal();
+
+document.querySelector("#form-create").addEventListener("submit",createPost);
+document.querySelector("#close-create-buttton").addEventListener("click",closeCreateDialog);
+}
+
+function closeCreateDialog(){
+
+  document.querySelector("#create-dialog").close();
+}
+
+
+
 
   function deleteClicked() {
     deletePost(post.id);
   }
 
-  function updateClicked() {
-    updatePost(post.id, title, body, image);
+  function updateClicked(event) {
+    event.preventDefault();
+
+    //resets content of form so input fields are blank
+    document.querySelector("#form-update").reset();
+
+    //shows modal dialog window
+    document.querySelector("#update").showModal();
+
+    //submits info in form when submit is cliked
+    document
+      .querySelector("#form-update")
+      .addEventListener("submit", submitClicked);
+
+    //makes closbutton in dialog clickable
+    document
+      .querySelector("#close-buttton")
+      .addEventListener("click", closeDialogClicked);
   }
+
+  function closeDialogClicked(event) {
+    //prevent default stops the button from submitting
+    event.preventDefault();
+    //closes the dialog without submitting data
+    document.querySelector("#update").close();
+
+    console.log("close clicked");
+  }
+
+  function submitClicked(event) {
+    event.preventDefault();
+    document.querySelector("#update").close();
+
+    console.log(event);
+    updatePost(post.id);
+  }
+}
+
+// === UPDATE (PUT) === //
+async function updatePost(id) {
+  const elements = document.querySelector("#form-update").elements;
+
+  const postToUpdate = {
+    image: elements.namedItem("image").value,
+    title: elements.namedItem("title").value,
+    body: elements.namedItem("body").value,
+  };
+  const postAsJson = JSON.stringify(postToUpdate);
+  // const url = `${endpoint}/posts/${id}.json`;
+
+  const response = await fetch(`${endpoint}/posts/${id}.json`, {
+    method: "PUT",
+    body: postAsJson,
+  });
+  const data = await response.json();
+
+  //is this right?
+  if (response.ok) {
+    updatePostsGrid();
+  }
+  console.log(data);
 }
 
 function showUsers(users) {
